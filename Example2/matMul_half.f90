73af4ea8b0f69e5ec7c51452ba91c87a9bb74d6f
Unknown <daniel.pecak@gmail.com>
2018-05-24 15:39:58 +0200

    Commenting Fortran testing (too long)

--- Example2/matMul_half.f90
+++ Example2/matMul_half.f90
@@ -15,10 +15,11 @@ end module matrix
 program matMul
   use ISO_C_BINDING
   use matrix
-  integer, parameter :: n=1000
+  integer, parameter :: n=2**13
   ! integer, parameter :: n=3
   integer :: i,j,k
-  real*8 :: a(n,n), b(n,n), c(n,n), cF(n,n)
+  real*8 :: a(n,n), b(n,n), c(n,n)
+  ! real*8 :: cF(n,n)
   do i=1,n
     do j=1,n
       a(i,j) = i*j
@@ -27,15 +28,16 @@ program matMul
   enddo
   print *, "Data initialized"
   call matmul_wrapper(n,a,b,c)
-  print *, "Sprawdzenie w fortranie"
-  do i=1,n
-    do j=1,n
-      cF(i,j) = 0.d0
-      do k=1,n
-        cF(i,j)=cF(i,j)+a(k,i)*b(j,k)
-      enddo
-    enddo
-  enddo
+  print *, "Koniec fortrana"
+  ! print *, "Sprawdzenie w fortranie"
+  ! do i=1,n
+  !   do j=1,n
+  !     cF(i,j) = 0.d0
+  !     do k=1,n
+  !       cF(i,j)=cF(i,j)+a(k,i)*b(j,k)
+  !     enddo
+  !   enddo
+  ! enddo
   ! print *, "Sprawdzenie"
   ! do i=1,n
   !   print "(3F8.2,A,3F8.2)", c(:,i), "        ", cF(i,:)
@@ -45,11 +47,11 @@ program matMul
   !   print "(3F8.2,A,3F8.2)", a(:,i), "        ", b(:,i)
   ! enddo
   ! print *, "========="
-  do i=1,n
-    do j=1,n
-      if(dabs(c(i,j)-cF(j,i))>0.1d0) then
-        print "(2I3,4F8.2)", i, j, a(i,j), b(i,j), c(j,i), cF(j,i)
-      endif
-    enddo
-  enddo
+  ! do i=1,n
+  !   do j=1,n
+  !     if(dabs(c(i,j)-cF(j,i))>0.1d0) then
+  !       print "(2I3,4F8.2)", i, j, a(i,j), b(i,j), c(j,i), cF(j,i)
+  !     endif
+  !   enddo
+  ! enddo
 end program matMul
